3bb4aa5e80af60eae6e5a20369f760b96b5d1ae2
vishal chaturvedi <64676999+vishal0862@users.noreply.github.com>
2022-10-25 22:56:37 +0530

    Update Abc.java

--- Abc.java
+++ Abc.java
@@ -4,6 +4,7 @@ public class Abc
 {
    public static void main(String[] args) {
       Scanner sc=new Scanner(System.in);
+      System.out.println("Enter any number");
       int a=sc.nextInt();
       for(int i=0; i<a; i++)
       {
