8dd417f8bdc8eb4e9ae24901e5edf1b8d833b55d
vishal chaturvedi <64676999+vishal0862@users.noreply.github.com>
2022-11-09 23:24:15 +0530

    Update Abc.java

--- Abc.java
+++ Abc.java
@@ -1,6 +1,6 @@
 public class Abc
 {
-   public static void main(String[] args) {
+   public static void main(String[] args) {;
       Scanner sc=new Scanner(System.in);
       System.out.println("Enter any number");
       int a=sc.nextInt();
